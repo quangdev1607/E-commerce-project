b691b3b449f7a110ab7b7e8c1acaab81cdce9cb0
quangdev1607 <quangphan1607@gmail.com>
2023-12-19 17:26:05 +0700

    update

--- server/server.js
+++ server/server.js
@@ -17,9 +17,19 @@ const connectDb = require("./src/config/db/connect");
 
 //-----------------GENERAL-----------------------
 app.use(cookieParser());
+const devOrigin = ["https://localhost:5173"];
+const prodOrigin = [process.env.CLIENT_URL];
+const allowedOrigin = process.env.NODE_ENV === "production" ? prodOrigin : devOrigin;
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+      if (allowedOrigin.includes(origin)) {
+        console.log(origin, allowedOrigin);
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["POST", "PUT", "GET", "DELETE"],
     credentials: true,
   })
